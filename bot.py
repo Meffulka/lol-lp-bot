11e6952db9a5666ac8e20fd1574a4a9a7cf85a96
AlekseiiErokhin <zmeffulka@gmail.com>
2018-09-03 09:39:39 +0300

    fixes:

--- bot.py
+++ bot.py
@@ -4,8 +4,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities 
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from base64 import b64decode
@@ -18,7 +16,8 @@ import datetime
 
 def findGift():
     try:
-        driver.find_element_by_xpath("/html/body/div[1]/div[2]/live-app/div/div/div/div/gifts-outlet/div/gift/div/div/div[4]/button")
+        driver.find_element_by_css_selector(".gift__button")
+        
         print("["+datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"]: найден подарок")
     except NoSuchElementException:
         print("[" + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"]: пусто")
@@ -37,7 +36,6 @@ options.add_argument("--headless")
 
 binary = FirefoxBinary('/usr/lib/firefox/firefox')
 
-#driver = webdriver.Remote("http://selenium:4444/wd/hub", desired_capabilities=options.to_capabilities())
 driver = webdriver.Firefox(firefox_options=options, firefox_binary=binary)
 driver.implicitly_wait(30)
 
@@ -46,7 +44,7 @@ driver.get("https://live.ru.lolesports.com/")
 
 
 
-driver.find_element_by_xpath("/html/body/div[1]/welcome-popup/div/slider/button").click()
+driver.find_element_by_css_selector(".slider__close").click()
 
 driver.find_element_by_xpath('//*[@id="riotbar-account"]/a[2]').click()
 
@@ -60,8 +58,7 @@ while True:
         time.sleep(10)
         driver.find_element_by_css_selector(".gift__button").click()
         screen = BytesIO(b64decode(driver.get_screenshot_as_base64()))
-        msg = "Подарок получен" 
-        print("["+datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"]:" + msg)
+        print("["+datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"]: Подарок получен")
         bot.send_photo(chat_id=chat_id, photo=screen)
         time.sleep(10)
         driver.find_element_by_css_selector(".gift__button").click()
